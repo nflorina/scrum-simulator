13634eaf719d6eea9a822c7b5dd18504541678a7
florina nastasoiu <florina.nastasoiu@yahoo.com>
2017-11-15 23:15:05 +0200

    Add files via upload

---
+++ Start.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hakathon;
+
+/**
+ *
+ * @author florence
+ */
+public class Start {
+    public static void main(String[] args) {
+        Home hm = new Home();
+        hm.setVisible(true);
+    }
+    
+}
